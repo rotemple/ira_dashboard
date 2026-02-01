a8f87c3692c54e8ea708833e5ea0ad9a2b7c3f80
Ryan Omizo <49911257+rotemple@users.noreply.github.com>
2026-02-01 16:16:17 -0500

    Update ira_streamlit.py

--- ira_streamlit.py
+++ ira_streamlit.py
@@ -60,70 +60,73 @@ def hashtag_extract(x):
             hashtags.append(ht)
 
     return hashtags
+
+st.set_page_config(page_title='Propaganda Data Browser')
+
 # Set the title of the Streamlit app
-st.title("📊 Data Dashboard to Browse fivethirtyeights' IRA Tweet Dataset")
+st.title("📊 Data Dashboard to Browse Propaganda Data")
 st.subheader('Fivethirtyeight/russian-troll-tweets. (2025). [Computer software]. FiveThirtyEight. https://github.com/fivethirtyeight/russian-troll-tweets (Original work published 2018)')
 
-option = st.sidebar.selectbox(
-    "Which file do you wish to browse?",
-    ('IRAhandle_tweets_1.csv','IRAhandle_tweets_2.csv','IRAhandle_tweets_3.csv',
-     'IRAhandle_tweets_4.csv','IRAhandle_tweets_5.csv','IRAhandle_tweets_6.csv',
-     'IRAhandle_tweets_7.csv','IRAhandle_tweets_8.csv','IRAhandle_tweets_9.csv',
-     'IRAhandle_tweets_10.csv','IRAhandle_tweets_11.csv','IRAhandle_tweets_12.csv',
-     'IRAhandle_tweets_13.csv',))
+# option = st.sidebar.selectbox(
+#     "Which file do you wish to browse?",
+#     ('IRAhandle_tweets_1.csv','IRAhandle_tweets_2.csv','IRAhandle_tweets_3.csv',
+#      'IRAhandle_tweets_4.csv','IRAhandle_tweets_5.csv','IRAhandle_tweets_6.csv',
+#      'IRAhandle_tweets_7.csv','IRAhandle_tweets_8.csv','IRAhandle_tweets_9.csv',
+#      'IRAhandle_tweets_10.csv','IRAhandle_tweets_11.csv','IRAhandle_tweets_12.csv',
+#      'IRAhandle_tweets_13.csv',))
 
-st.sidebar.write('See https://github.com/rotemple/russian-troll-tweets for the complete dataset description.')
+#st.sidebar.write('See https://github.com/rotemple/russian-troll-tweets for the complete dataset description.')
 # File uploader widget in the sidebar
 
-url = 'https://raw.githubusercontent.com/rotemple/russian-troll-tweets/refs/heads/master/'
-df = pd.read_csv(url+option)
+#url = 'https://raw.githubusercontent.com/rotemple/russian-troll-tweets/refs/heads/master/'
+#df = pd.read_csv(url+option)
 
-df['col1'] = list(range(len(df)))
-tweets = df.content.tolist()
+# df['col1'] = list(range(len(df)))
+# tweets = df.content.tolist()
 
     
     # Display a preview of the data
-container = st.container()    
-container.subheader("Data Preview: " + option)
-container.dataframe(df)
-container.subheader("Data Preview: " + option)
+# container = st.container()    
+# container.subheader("Data Preview: " + option)
+# container.dataframe(df)
+# container.subheader("Data Preview: " + option)
 
-wordcloud = WordCloud().generate(' '.join(cleaned))
+# wordcloud = WordCloud().generate(' '.join(cleaned))
 
-plt.axis("off")
+# plt.axis("off")
 
-fig = plt.figure()
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
+# fig = plt.figure()
+# plt.imshow(wordcloud, interpolation="bilinear")
+# plt.axis("off")
 
 
-container.pyplot(fig)
+# container.pyplot(fig)
     
-    # Display basic statistics
-container.subheader("Descriptive Statistics: " + option)
-container.write(df.describe())
+#     # Display basic statistics
+# container.subheader("Descriptive Statistics: " + option)
+# container.write(df.describe())
 
-st.subheader('Top-50 Hashtags for  '+option)
-try:
-    hashtags = flatten_list(flatten_list([hashtag_extract(tweet) for tweet in tweets]))
-    hcounts = pd.DataFrame(Counter(hashtags).most_common()[:50])
-    hcounts = hcounts.rename(columns={0:'hashtag',1:'count'})
+# st.subheader('Top-50 Hashtags for  '+option)
+# try:
+#     hashtags = flatten_list(flatten_list([hashtag_extract(tweet) for tweet in tweets]))
+#     hcounts = pd.DataFrame(Counter(hashtags).most_common()[:50])
+#     hcounts = hcounts.rename(columns={0:'hashtag',1:'count'})
     
-    st.dataframe(hcounts) 
-except:
-    st.subheader("hashtag extraction error!")
+#     st.dataframe(hcounts) 
+# except:
+#     st.subheader("hashtag extraction error!")
 
-#display top-50 mentions
-st.subheader('Top-50 Mentions for  '+option)
+# #display top-50 mentions
+# st.subheader('Top-50 Mentions for  '+option)
 
-try:
-    mentions =flatten_list(flatten_list([mention_extract(tweet) for tweet in tweets]))
-    counts = pd.DataFrame(Counter(mentions).most_common()[:50])
-    counts = counts.rename(columns={0:'mention',1:'count'})
+# try:
+#     mentions =flatten_list(flatten_list([mention_extract(tweet) for tweet in tweets]))
+#     counts = pd.DataFrame(Counter(mentions).most_common()[:50])
+#     counts = counts.rename(columns={0:'mention',1:'count'})
     
-    st.dataframe(counts)    
-except:
-    st.subheader('mention extraction error!')
+#     st.dataframe(counts)    
+# except:
+#     st.subheader('mention extraction error!')
 
 
 container.write('Streamlit app created by Ryan Omizo')    
