02375b57d083422fd0124d80ffb854d77d22f43e
Ryan Omizo <49911257+rotemple@users.noreply.github.com>
2026-04-06 10:39:32 -0400

    Update and rename 6_irads to 6_irads.py

---
+++ pages/6_irads.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import re
+import itertools
+from collections import Counter
+
+def flatten_list(somelist):
+        if any(isinstance(el, list) for el in somelist) == False:
+            return somelist
+        flat_list = list(itertools.chain(*somelist))
+        return flat_list
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def search_dataframe(dataframe,query):
+    dataframe['content'] = dataframe.content.str.lower()
+    dataframe['search'] = dataframe.content.str.contains(query)
+    return dataframe[dataframe['search'] == True]
+
+@st.cache_data
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+@st.cache_data
+def load_csv(url):
+  return pd.read_csv(url,usecols=['author','content','following','followers','account_type','account_category','publish_date','post_type'])
+  
+df = load_csv('https://raw.githubusercontent.com/rotemple/irads/refs/heads/master/site/index.csv')
+
+st.write(df)
+
+
+
